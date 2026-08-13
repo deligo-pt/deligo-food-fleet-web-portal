3a2e2962454a5e66853ef7421889d1d82e581d6c
MoRsHeD <mdmorshed0187@gmail.com>
2026-08-13 15:44:44 +0600

    added no auto translation logics in layout.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" translate="no">
       <body className={`antialiased`}>
         <GoogleMapsProvider>
           {children}
